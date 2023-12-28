2acc67d6dda55e4069b55525300aab92dc927152
yasmeenhelal <yasminehilal99@gmail.com>
2023-12-29 01:53:25 +0200

    final

--- Frontend/my-app/src/components/Navbar.js
+++ Frontend/my-app/src/components/Navbar.js
@@ -1,25 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === '/';
+  const isSignupPage = location.pathname === '/signup';
+
   return (
     <header>
       <div className="container">
         <Link to="/">
-          <h1 style={{ color: 'red' }}>Botit Dashboard</h1>
+          <h1 style={{ color: 'red' }}>Admin Botit Dashboard</h1>
         </Link>
 
         <div className="nav-buttons">
-          <Link to="/products" className="nav-button">
+          <Link
+            to="/products"
+            className={`nav-button ${isLoginPage || isSignupPage ? 'disabled' : ''}`}
+          >
             Products
           </Link>
-          <Link to="/home" className="nav-button">
+          <Link
+            to="/home"
+            className={`nav-button ${isLoginPage || isSignupPage ? 'disabled' : ''}`}
+          >
             Orders
           </Link>
         </div>
       </div>
     </header>
   );
-}
+};
 
 export default Navbar;
